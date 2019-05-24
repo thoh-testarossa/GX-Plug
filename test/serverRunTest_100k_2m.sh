43f52512ff5d1c1ac278ac0138e87499d88f3af3
Kamosphere <79625970@qq.com>
2019-05-24 14:34:09 +0800

    submodule test

--- test/serverRunTest_100k_2m.sh
+++ test/serverRunTest_100k_2m.sh
@@ -3,3 +3,4 @@ nohup ./srv_UtilServerTest_BellmanFordGPU 100000 500000 3 0 &
 nohup ./srv_UtilServerTest_BellmanFordGPU 100000 500000 3 1 &
 nohup ./srv_UtilServerTest_BellmanFordGPU 100000 500000 3 2 &
 nohup ./srv_UtilServerTest_BellmanFordGPU 100000 500000 3 3 &
+
